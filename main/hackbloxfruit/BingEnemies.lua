e81e3d07e47bd7325febd69b85e6d4a0227fc2fd
lumakd <144311544+lumakd@users.noreply.github.com>
2025-01-01 14:47:46 +0700

    Update BingEnemies.lua

--- main/hackbloxfruit/BingEnemies.lua
+++ main/hackbloxfruit/BingEnemies.lua
@@ -1,38 +1,35 @@
- function Module:BringEnemies(ToEnemy: Instance, SuperBring: boolean?): (nil)
-    if not self.IsAlive(ToEnemy) or not ToEnemy.PrimaryPart then
-      return nil
+function Module:BringEnemies(ToEnemy: Instance, SuperBring: boolean?): (nil)
+if not self.IsAlive(ToEnemy) or not ToEnemy.PrimaryPart then
+    return nil
+end
+
+pcall(sethiddenproperty, Player, "SimulationRadius", math.huge)
+
+if Settings.BringMobs then
+    local Name = ToEnemy.Name
+    local Position = (Player.Character or Player.CharacterAdded:Wait()):GetPivot().Position
+    local Target = ToEnemy.PrimaryPart.CFrame
+    
+    if not CachedBring[Name] or (Target.Position - CachedBring[Name].Position).Magnitude > 5 then
+    CachedBring[Name] = Target
     end
-    
-    pcall(sethiddenproperty, Player, "SimulationRadius", math.huge)
-    
-    if Settings.BringMobs then
-      local Name = ToEnemy.Name
-      local Position = (Player.Character or Player.CharacterAdded:Wait()):GetPivot().Position
-      local Target = ToEnemy.PrimaryPart.CFrame
-      
-      if not CachedBring[Name] or (Target.Position - CachedBring[Name].Position).Magnitude > 5 then
-        CachedBring[Name] = Target
-      end
-      
-      for _, Enemy in ipairs(SuperBring and Enemies:GetChildren() or self.allMobs[Name]) do
+    for _, Enemy in ipairs(SuperBring and Enemies:GetChildren() or self.allMobs[Name]) do
         if Enemy:HasTag(BRING_TAG) then continue end
-        
         local PrimaryPart = Enemy.PrimaryPart
         if self.IsAlive(Enemy) and PrimaryPart then
-          if (Position - PrimaryPart.Position).Magnitude < Settings.BringDistance then
-            PrimaryPart.Size = HitBoxSize
-            PrimaryPart.CanCollide = false
-            Enemy.Humanoid.WalkSpeed = 0
-            Enemy.Humanoid.JumpPower = 0
-            Enemy:AddTag(BRING_TAG)
-          end
+            if (Position - PrimaryPart.Position).Magnitude < Settings.BringDistance then
+                PrimaryPart.Size = HitBoxSize
+                PrimaryPart.CanCollide = false
+                Enemy.Humanoid.WalkSpeed = 0
+                Enemy.Humanoid.JumpPower = 0
+                Enemy:AddTag(BRING_TAG)
+            end
         end
-      end
-    else
-      if not CachedBring[ToEnemy] then
-        CachedBring[ToEnemy] = ToEnemy.PrimaryPart.CFrame
-      end
-      
-      ToEnemy.PrimaryPart.CFrame = CachedBring[ToEnemy]
     end
-  end
+    else
+        if not CachedBring[ToEnemy] then
+            CachedBring[ToEnemy] = ToEnemy.PrimaryPart.CFrame
+        end
+    ToEnemy.PrimaryPart.CFrame = CachedBring[ToEnemy]
+    end
+end
