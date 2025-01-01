0354fc23dbec3a4954e718b0842b985da8df8ec1
lumakd <144311544+lumakd@users.noreply.github.com>
2025-01-01 13:36:07 +0700

    Update VerifyToolTip.lua

--- main/hackbloxfruit/VerifyToolTip.lua
+++ main/hackbloxfruit/VerifyToolTip.lua
@@ -1,13 +1,27 @@
-function VerifyToolTip(Type: string): Instance?
-  local Cached = CachedTools["Tip_" .. Type]
-  
-  if Cached and Cached.Parent then
-    return Cached
-  end
-  
-  for _, Tool in Player.Backpack:GetChildren() do
-    if Tool:IsA("Tool") and Tool.ToolTip == Type then
-      CachedTools["Tip_" .. Type] = Tool
-      return Tool
+  function VerifyToolTip(Type: string): Instance?
+    local Cached = CachedTools["Tip_" .. Type]
+    
+    if Cached and Cached.Parent then
+      return Cached
     end
+    
+    for _, Tool in Player.Backpack:GetChildren() do
+      if Tool:IsA("Tool") and Tool.ToolTip == Type then
+        CachedTools["Tip_" .. Type] = Tool
+        return Tool
+      end
+    end
+    
+    if not Module.IsAlive(Player.Character) then
+      return nil
+    end
+    
+    for _, Tool in Player.Character:GetChildren() do
+      if Tool:IsA("Tool") and Tool.ToolTip == Type then
+        CachedTools["Tip_" .. Type] = Tool
+        return Tool
+      end
+    end
+    
+    return nil
   end
